12e48aa736f3481f0ef5ed8351d961dc6de29374
thasup <thanachonfirst@hotmail.com>
2022-01-09 02:51:54 +0700

    JavaScript concatenation

---
+++ docs/js/all.js
@@ -0,0 +1,11 @@
+function getWindowHeight() {
+	return window.innerHeight;
+}
+
+getWindowHeight();
+(function() {
+
+	var foo = 1;
+	return foo;
+
+})();
--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,35 @@
-const { watch, dest, src, series } = require("gulp");
+const { watch, dest, src, series, parallel } = require("gulp");
 const sass = require("gulp-sass")(require("sass"));
 const browserSync = require("browser-sync").create();
 const eslint = require("gulp-eslint");
 const jasmineBrowser = require("gulp-jasmine-browser");
+const concat = require("gulp-concat");
 
 function styles() {
     return src("sass/**/*.scss")
-        .pipe(sass().on("error", sass.logError))
-        .pipe(dest("./css"))
+        .pipe(sass({ outputStyle: "compressed" }).on("error", sass.logError))
+        .pipe(dest("./docs/css"))
         .pipe(browserSync.stream());
 }
 
+function html() {
+    return src("./index.html").pipe(dest("./docs"));
+}
+
+function images() {
+    return src("./images/*").pipe(dest("./docs/images"));
+}
+
+function scripts() {
+    return src("./js/**/*.js").pipe(concat("all.js")).pipe(dest("./docs/js"));
+}
+
+function scriptsDist() {
+    return src("./js/**/*.js").pipe(concat("all.js")).pipe(dest("./docs/js"));
+}
+
 function lint() {
-    return src(["js/**/*.js"])
+    return src(["./js/**/*.js"])
         .pipe(eslint())
         .pipe(eslint.format())
         .pipe(eslint.failOnError());
@@ -31,11 +48,16 @@ function test() {
 }
 
 exports.styles = styles;
+exports.lint = lint;
 exports.test = test;
+exports.build = parallel(images, series(html, styles, scripts));
 exports.default = function () {
-    series(styles, lint, test);
-    watch("sass/**/*.scss", styles);
+    parallel(images, series(html, styles));
+
+    watch("./sass/**/*.scss", styles);
+    watch("./js/**/*.js", lint);
+    watch("./index.html", html);
     browserSync.init({
-        server: "./",
+        server: "./docs",
     });
 };
