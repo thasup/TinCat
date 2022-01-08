0aac82a95592328c3b1bb0c483af477a39201567
thasup <thanachonfirst@hotmail.com>
2022-01-09 01:38:03 +0700

    mitigate to gulp v4

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,18 @@
-const gulp = require("gulp");
+const { watch, dest, src } = require("gulp");
 const sass = require("gulp-sass")(require("sass"));
+const browserSync = require("browser-sync").create();
 
-gulp.task("default", function () {
-    console.log("Hello World");
-});
-
-gulp.task("styles", function () {
-    gulp.src("sass/**/*.scss")
+function styles() {
+    return src("sass/**/*.scss")
         .pipe(sass().on("error", sass.logError))
-        .pipe(gulp.dest("./css"));
-});
+        .pipe(dest("./css"))
+        .pipe(browserSync.stream());
+}
+
+exports.styles = styles;
+exports.default = function () {
+    watch("sass/**/*.scss", styles);
+    browserSync.init({
+        server: "./",
+    });
+};
